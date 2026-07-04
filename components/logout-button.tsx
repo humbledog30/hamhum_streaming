8d5d211accdeb647a00f09eb52ba2363cc236ea9
humbledog30 <mikepialago@gmail.com>
2026-07-05 02:23:43 +0800

    Layout - Add navigation

--- components/logout-button.tsx
+++ components/logout-button.tsx
@@ -5,13 +5,17 @@ import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
 
 export function LogoutButton() {
-  const router = useRouter();
+	const router = useRouter();
 
-  const logout = async () => {
-    const supabase = createClient();
-    await supabase.auth.signOut();
-    router.push("/auth/login");
-  };
+	const logout = async () => {
+		const supabase = createClient();
+		await supabase.auth.signOut();
+		router.push("/auth/login");
+	};
 
-  return <Button onClick={logout}>Logout</Button>;
+	return (
+		<Button className="rounded-2xl px-5" size="sm" variant={"outline"} onClick={logout}>
+			Logout
+		</Button>
+	);
 }
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  cacheComponents: true,
+	cacheComponents: true,
+	allowedDevOrigins: ["192.168.0.101", "192.168.0.103"],
 };
 
 export default nextConfig;
