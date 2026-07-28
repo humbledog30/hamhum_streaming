ef4b5cd19b6f48962f15ac9b2933d81c9cb5d543
humbledog30 <mikepialago@gmail.com>
2026-07-28 20:22:02 +0800

    Quick fix - Issue on moview credit props

--- app/(user)/(default)/browse/components/skeleton-loader/browse-page-skeleton.tsx
+++ app/(user)/(default)/browse/components/skeleton-loader/browse-page-skeleton.tsx
@@ -4,10 +4,10 @@ export const BrowsePageSkeleton = () => {
 	return (
 		<>
 			<section className="relative w-full">
-				<div className="absolute inset-0 bg-background">
+				{/* <div className="absolute inset-0 bg-background">
 					<div className="absolute inset-0 bg-[radial-gradient(ellipse_1200px_800px_at_15%_0%,hsl(var(--primary)/0.30),transparent_60%)]" />
 					<div className="absolute inset-x-0 bottom-0 h-40 bg-linear-to-b from-transparent to-background" />
-				</div>
+				</div> */}
 				<div className="section-container relative z-10 flex flex-col gap-y-4 pt-30 pb-10">
 					<div className="flex items-center gap-3">
 						<Skeleton className="h-6 w-6 rounded-full" />
